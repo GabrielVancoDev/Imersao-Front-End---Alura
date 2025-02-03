4d51d6ff01aca56366f558cb323b4a5a529431aa
Natiele <142361378+natieledpaula@users.noreply.github.com>
2025-02-03 15:02:44 -0300

    Fim

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlist');
 
-function requestApi(searchTerm) {
-    const url = `http://localhost:3000/search?name_likes=${searchTerm}`
-    fetch(url)
-    .then((response) => response.json())
-    .then((result) => displayResults(result))
-}
+//BOM DIA | BOA TARDE | BOA NOITE
 
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+// Obtém a referência do elemento com o ID "greeting"
+const greetingElement = document.getElementById("greeting");
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
-
-    resultArtist.classList.remove('hidden');
-}
-
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
-    }
-    requestApi(searchTerm);
-});
-
-const greetingElement = document.getElementById("dreeting");
+// Obtém a hora atual do sistema
 const currentHour = new Date().getHours();
-const greetingMessage =
-currentHour >= 5 && currentHour < 12 
-? "Bom dia"
-: currentHour >= 12 && currentHour < 18
-? "Boa tarde"
-? "Boa noite";
 
+// Define a saudação com base na hora atual
+// if (currentHour >= 5 && currentHour < 12) {
+//   greetingElement.textContent = "Bom dia";
+// } else if (currentHour >= 12 && currentHour < 18) {
+//   greetingElement.textContent = "Boa tarde";
+// } else {
+//   greetingElement.textContent = "Boa noite";
+// }
+
+// Forma mais simples
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+// GRID INTELIGENTE
 const container = document.querySelector(".offer__list-item");
 
-const observer = new ResizeObserver(() => {  
-  const containerWidth = container.offsetWidth; 
-  const numColumns = Math.floor(containerWidth / 200); 
+const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
+  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
 
+  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
   container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 
   console.log({ container });
   console.log({ numColumns });
 });
-
-observer.observe(container); 
+//observando a mudança do elemento
+observer.observe(container);
